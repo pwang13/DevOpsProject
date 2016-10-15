7d042447afe66911035bc85876fdd1e380c00e72
pwang13 <pwang13@ncsu.edu>
2016-10-15 19:57:35 -0400

    coverage

---
+++ cover.js
@@ -0,0 +1,23 @@
+var fs = require('fs');
+
+fs.readFile('test.txt', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  // console.log(data);
+  lines = data.split("\n");
+  // console.log(lines[1]);
+  // console.log(lines);
+  for (var i in lines) {
+
+  		// console.log(lines[i]);
+  	if (lines[i].includes("Statement")) {
+  		var j = lines[i].indexOf(':');
+  		var k = lines[i].indexOf('%');
+  		// console.log(lines[i]);
+  		// console.log(j);
+  		var n = parseFloat(lines[i].substring(j + 1,k));
+  		console.log(n > 90);
+  	}
+  }
+});
